a21ee3396c99f7b09cf76a0f33799948a55c7275
Haruka Ashiuchi <chopperman1224@gmail.com>
2022-09-28 14:28:36 +0900

    front-pageに記事タイトルを表示する

--- front-page.php
+++ front-page.php
@@ -1,9 +1,22 @@
-<?php 
-/*
-Template Name: Top page
-*/
-?>
+<!DOCTYPE html>
+<html lang="ja">
 
-<main>
-  <h2>Hello World</h2>
-</main>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <?php wp_body_open(); ?>
+  <main>
+    <?php if(have_posts()): while(have_posts()): the_post(); ?>
+    <article <?php post_class(); ?>>
+      <h2><?php the_title(); ?></h2>
+    </article>
+    <?php endwhile; endif; ?>
+  </main>
+  <?php wp_footer(); ?>
+</body>
+
+</html>
