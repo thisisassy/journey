0c01672ce80df7cc31d2970920d2fff02aafbb03
Haruka Ashiuchi <chopperman1224@gmail.com>
2022-09-29 21:01:36 +0900

    entryの要素をスタイルする

--- single.php
+++ single.php
@@ -7,13 +7,15 @@
       <div class="o-entryHead">
         <h2 class="u-entryTitle"><?php the_title(); ?></h2>
         <div class="o-meta">
-          <p>category</p>
+          <?php the_category(); ?>
           <p class="u-dateTime"><time
               datetime="<?php echo esc_attr(get_the_date(DATE_W3C)); ?>"><?php echo esc_html(get_the_date()); ?></time>
           </p>
         </div>
       </div>
-      <?php the_content(); ?>
+      <div class="l-entryContent">
+        <?php the_content(); ?>
+      </div>
       <?php endwhile; endif; ?>
       <div class="o-author">
         <p>この記事を書いた人</p>
