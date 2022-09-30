c036a7fd776963616ce2818781920d722b0e7edc
Haruka Ashiuchi <chopperman1224@gmail.com>
2022-09-30 23:40:28 +0900

    サイドバーのスタイルを整える

--- footer.php
+++ footer.php
@@ -1,15 +1,16 @@
 <footer class="l-footer">
   <div class="l-footerOuter">
+    <?php if(is_active_sidebar('sidebar-2')): ?>
+    <?php dynamic_sidebar('sidebar-2'); ?>
+    <?php endif; ?>
   </div>
   <div class="l-footerInner">
+    <?php if(is_active_sidebar('sidebar-3')): ?>
+    <?php dynamic_sidebar('sidebar-3'); ?>
+    <?php endif; ?>
     <p class="u-copyright"><span>&copy;</span>2022
       <?php bloginfo('name'); ?>
     </p>
-    <ul class="o-footerLinks">
-      <li><a href="">プライバシーポリシー</a></li>
-      <li><a href="">お問い合わせ</a></li>
-      <li><a href="">運営者情報</a></li>
-    </ul>
   </div>
 </footer>
 <?php wp_footer(); ?>
